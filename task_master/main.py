2a1c539491c50ef9709f6a4ac8d837df8d04c114
Tring-Er <tringalieros@gmail.com>
2023-01-30 11:14:43 +0100

    Temporary removed GUI

--- task_master/main.py
+++ task_master/main.py
@@ -1,18 +1,26 @@
 """Start of project"""
 
+#from sys import argv as sys_argv
+
 from model.task_manager import TaskManager
-#from view.cli import CLI
-from view.gui import GUI
-#from presenter.console_manager import ConsoleManager
-from presenter.gui_manager import GUIManager
+from view.cli import CLI
+# from view.gui import GUI
+from presenter.console_manager import ConsoleManager
+# from presenter.gui_manager import GUIManager
 
 
 def main() -> None:
     """Main func"""
 
     model = TaskManager()
-    view = GUI()  # CLI()
-    presenter = GUIManager(model, view)  # ConsoleManager(model, view)
+    #if len(sys_argv) > 1:
+    #    view = CLI()
+    #    presenter = ConsoleManager(model, view)
+    #else:
+    #    view = GUI()
+    #    presenter = GUIManager(model, view)
+    view = CLI()
+    presenter = ConsoleManager(model, view)
     presenter.compute()
 
 
