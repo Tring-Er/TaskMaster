4b237069a571cb68d40f84e6e3a8e53f28e42968
Tring-Er <tringalieros@gmail.com>
2023-04-19 11:25:36 +0200

    Created Window object (wrapper arround tkinter.TK)

--- task_master/details/GUI/widgets/Widget.py
+++ task_master/details/GUI/widgets/Widget.py
@@ -6,3 +6,11 @@ class Widget(ABC):
     @abstractmethod
     def add_widget(self, widget: any) -> None:
         """Adds a widget inside this widget"""
+    
+    @abstractmethod
+    def create(self, **kwargs: dict[str: any]) -> None:
+        """Create the widget of the library"""
+    
+    @abstractmethod
+    def show(self) -> None:
+        """Shows the widgets on the screen (and show the widget inside this one as well)"""
--- task_master/details/GUI/widgets/Window.py
+++ task_master/details/GUI/widgets/Window.py
@@ -7,14 +7,27 @@ class Window(Widget):
     """Wrapper arround Tk object"""
     
     def __init__(self, title: str, icon_path: str, percentage_of_screen_width_taken_by_window: float, percentage_of_screen_height_taken_by_window: float) -> None:
-        self.tk_object = Tk()
-        widgets: list[Widget] = []
-        self.set_geometry(percentage_of_screen_width_taken_by_window, percentage_of_screen_height_taken_by_window)
-        self.set_title(title)
-        self.set_icon(icon_path)
+        self.tk_object = None
+        self.widgets: list[Widget] = []
+        self.size = (percentage_of_screen_width_taken_by_window, percentage_of_screen_height_taken_by_window)
+        self.title = title
+        self.icon_path = icon_path
     
-    def add_widget(self, widget: any) -> None:
-        ...
+    def add_widget(self, widget: Widget) -> None:
+        self.widgets.append(widget)
+    
+    def create(self, **kwargs: dict[str: any]) -> None:
+        self.tk_object = Tk()
+        self.set_geometry(*self.size)
+        self.set_title(self.title)
+        self.set_icon(self.icon_path)
+        for widget in self.widgets:
+            widget.create(parent=self.tk_object)
+    
+    def show(self) -> None:
+        for widget in self.widgets:
+            widget.show()
+        self.tk_object.mainloop()
     
     def set_geometry(self, percentage_of_screen_width_taken_by_window: float, percentage_of_screen_height_taken_by_window: float) -> None:
         screen_width = self.tk_object.winfo_screenwidth()
